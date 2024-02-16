2635e1a46000fdea50e111497da3413ae25539dd
trylow10 <aryaltrylowchan@gmail.com>
2024-02-16 10:16:28 +0545

    added: ngrok sdk

--- build/script.js
+++ build/script.js
@@ -1,14 +1,27 @@
 import dotenv from "dotenv";
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
+// import { ngrockInnit } from "./ngrok.js";
+
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/webhook", (req, res) => {
-  const payload = req.body;
-  console.log("Received webhook payload:", payload);
-  // Process the payload here
-  res.status(200).send("Webhook received successfully");
+// const urlListner = await ngrockInnit();
+// console.log(`Ingress established at: ${urlListner.url()}`);
+
+// Add a route to handle the payload
+app.all("/", (req, res) => {
+  // Access payload data from request body
+  const payloadData = req.body;
+
+  // Do something with the payload data
+  console.log("Payload data:", payloadData);
+
+  // Send response
+  res.status(200).send("Payload received successfully.");
 });
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
