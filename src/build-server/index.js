f5d3f749661f818f572f2982b8d1b2946dbfa2a1
trylow10 <aryaltrylowchan@gmail.com>
2024-02-25 09:53:14 +0545

    Refactor build server code and handle errors

--- src/build-server/index.js
+++ src/build-server/index.js
@@ -3,21 +3,29 @@ import express from "express";
 import cors from "cors";
 import { innit } from "./executeScript.js";
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-export let DIR_NAME;
-const handler = async(req,res) =>{
-  const repoUrl = req.body.GIT_URL;
-  await innit(repoUrl);
-  DIR_NAME = repoUrl.split("/").slice(-1)[0].replace(".git", "");
-res.json({
-  msg:DIR_NAME
-})
 
-}
-app.post("/deploy", handler)
+export const getDirName = (repoUrl) => {
+  return repoUrl.split("/").slice(-1)[0].replace(".git", "");
+};
 
+const handler = async (req, res) => {
+  try {
+    const repoUrl = req.body.GIT_URL;
+    await innit(repoUrl);
+    const dirName = getDirName(repoUrl);
+    res.json({
+      msg: dirName,
+    });
+  } catch (error) {
+    console.error("Error handling request:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+app.post("/deploy", handler);
 app.listen(PORT, () => {
-  console.log(`server is running${PORT}`);
+  console.log(`Server is running at ${PORT}`);
 });
