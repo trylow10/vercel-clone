e52ca6d87bee5643e5c676dca6ec1b14d7a2094b
trylow10 <aryaltrylowchan@gmail.com>
2024-02-25 09:53:31 +0545

    Refactor getAllFiles function to include repository name in file paths

--- src/build-server/getItems.js
+++ src/build-server/getItems.js
@@ -1,31 +1,41 @@
 import path from "node:path";
 import fs from "node:fs";
-import { DIR_NAME } from "./index.js";
-export const getAllFiles = async () => {
-  const currentModuleDir = path.dirname(new URL(import.meta.url).pathname);
-  const distPath = path.join(
-    currentModuleDir,
-    "../../../../trilo/output",
-    DIR_NAME,
+import { getDirName } from "./index.js";
+
+export const getAllFiles = async (repoUrl) => {
+  const currentModuleDirectory = path.dirname(
+    new URL(import.meta.url).pathname
+  );
+  const baseOutputDirectory = "../../../../trilo/output/";
+  const repositoryName = await getDirName(repoUrl);
+  const distDirectory = path.join(
+    currentModuleDirectory,
+    baseOutputDirectory,
+    repositoryName,
     "dist"
   );
 
+  const files = [];
   try {
-    readDirectory(distPath);
+    readDirectory(distDirectory, repositoryName, files);
+    return files;
   } catch (error) {
     console.error("Error reading directory:", error);
   }
 };
 
-function readDirectory(directory) {
-  const distContent = fs.readdirSync(directory, { withFileTypes: true });
+function readDirectory(directory, repositoryName, files) {
+  const directoryContents = fs.readdirSync(directory, { withFileTypes: true });
 
-  for (const dirent of distContent) {
-    const fullPath = path.join(directory, dirent.name);
-    if (dirent.isDirectory()) {
-      readDirectory(fullPath);
+  for (const item of directoryContents) {
+    const fullPath = path.join(directory, item.name);
+    if (item.isDirectory()) {
+      readDirectory(fullPath, repositoryName, files);
     } else {
-      console.log(fullPath);
+      const filePath = `/${repositoryName}/${
+        fullPath.split(`/output/${repositoryName}/`)[1]
+      }`;
+      files.push(filePath);
     }
   }
 }
