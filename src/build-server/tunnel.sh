e5ac137d1fdcb720af6102378d7e21c5a400c895
trylow10 <aryaltrylowchan@gmail.com>
2024-02-28 21:07:32 +0545

    Add Dockerfile and example.env for build server

--- src/build-server/tunnel.sh
+++ src/build-server/tunnel.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Export the GIT_URL environment variable
 export GIT_URL="$1"
-# Export the DIR_NAME environment variable
 export DIR_NAME=$(basename "$GIT_URL" .git)
-# Clone the repository
 git clone "$GIT_URL" /home/trilo/output/"${DIR_NAME}"
-# Navigate to the cloned repository directory
 cd /home/trilo/output/"${DIR_NAME}" || exit
-# Install dependencies
 npm install --force && npm run build
 # Start the Node.js server
 # node index.js
